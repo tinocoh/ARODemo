d964ad2916c31e65b998c289a372eb977c97ea18
Roger Oliva <jaronet@hotmail.com>
2023-04-21 10:28:13 -0600

    .
    .

--- DeployScripts/creacion.sh
+++ DeployScripts/creacion.sh
@@ -1,11 +1,9 @@
 #/bin/bash
-export LOCATION=southcentralus
+export LOCATION=westus3
 export RESOURCEGROUP="arogbb$LOCATION"
 export CLUSTER="arocluster$LOCATION"
 # Optener Regiones
 az account list-locations
 # Optener VM list
 #az vm list-sizes -l southcentralus
-az aro create --resource-group $RESOURCEGROUP --name $CLUSTER --vnet aro-vnet --master-subnet master-subnet --worker-subnet worker-subnet --apiserver-visibility Public --ingress-visibility Public --domain foo.example.com --pull-secret @pull-secret.txt --worker-vm-size Standard_D8as_v4
-
-UPDATE.
+az aro create --resource-group $RESOURCEGROUP --name $CLUSTER --vnet aro-vnet --master-subnet master-subnet --worker-subnet worker-subnet --apiserver-visibility Public --ingress-visibility Public --domain jaropro.net --pull-secret @pull-secret.txt --worker-vm-size Standard_D8as_v4
