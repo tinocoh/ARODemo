632730662763d139d600fe364858c018072371e2
Roger Oliva <rooliva@microsoft.com>
2024-11-04 00:29:29 -0600

    update

--- DeployScripts/creacion.sh
+++ DeployScripts/creacion.sh
@@ -8,4 +8,6 @@ az account list-locations
 #az vm list-sizes -l southcentralus
 az aro create --resource-group $RESOURCEGROUP --name $CLUSTER --vnet aro-vnet --master-subnet master-subnet --worker-subnet worker-subnet --apiserver-visibility Public --ingress-visibility Public --domain foo.example.com --pull-secret @pull-secret.txt --worker-vm-size Standard_D8as_v4
 
-UPDATE.
+az aro create --resource-group arogbbwestus3 --name arolatamgbb --vnet aro-vnet --master-subnet master-subnet --worker-subnet worker-subnet --apiserver-visibility Public --ingress-visibility Public --domain aroarolatamgbb.jaropro.net --pull-secret @pull-secret.txt --worker-vm-size Standard_D8as_v4
+
+az aro create --resource-group arogbbwestus3 --name arolatamgbb --vnet aro-vnet --master-subnet master-subnet --worker-subnet worker-subnet --apiserver-visibility Private --ingress-visibility Private --domain aroarolatamgbb.jaropro.net --pull-secret @pull-secret.txt
--- DeployScripts/requerimeintos.sh
+++ DeployScripts/requerimeintos.sh
@@ -1,5 +1,6 @@
 #/bin/bash
-export LOCATION=southcentralus
+az login --use-device-code --tenant 16b3c013-d300-468d-ac64-7eda0820b6d3
+export LOCATION="westus3"
 export RESOURCEGROUP="arogbb$LOCATION"
 export CLUSTER="arocluster$LOCATION"
 az group create --name $RESOURCEGROUP --location $LOCATION
@@ -7,3 +8,19 @@ az network vnet create --resource-group $RESOURCEGROUP --name aro-vnet --address
 az network vnet subnet create --resource-group $RESOURCEGROUP --vnet-name aro-vnet --name master-subnet --address-prefixes 192.168.192.0/25 --service-endpoints Microsoft.ContainerRegistry
 az network vnet subnet create --resource-group $RESOURCEGROUP --vnet-name aro-vnet --name worker-subnet --address-prefixes 192.168.192.128/25 --service-endpoints Microsoft.ContainerRegistry
 az network vnet subnet update --name master-subnet --resource-group $RESOURCEGROUP --vnet-name aro-vnet --disable-private-link-service-network-policies true
+
+Powershell
+
+#/bin/bash
+az login --use-device-code --tenant 16b3c013-d300-468d-ac64-7eda0820b6d3
+export LOCATION="westus3"
+export RESOURCEGROUP="arogbbwestus3"
+export CLUSTER="aroclusterwestus3"
+az group create --name arogbbwestus3 --location westus3
+az network vnet create --resource-group arogbbwestus3 --name aro-vnet --address-prefixes 192.168.192.0/24
+az network vnet subnet create --resource-group arogbbwestus3 --vnet-name aro-vnet --name master-subnet --address-prefixes 192.168.192.0/25 --service-endpoints Microsoft.ContainerRegistry
+az network vnet subnet create --resource-group arogbbwestus3 --vnet-name aro-vnet --name worker-subnet --address-prefixes 192.168.192.128/25 --service-endpoints Microsoft.ContainerRegistry
+az network vnet subnet update --name master-subnet --resource-group arogbbwestus3 --vnet-name aro-vnet --disable-private-link-service-network-policies true
+az aro create --resource-group arogbbwestus3 --name arolatamgbb --vnet aro-vnet --master-subnet master-subnet --worker-subnet worker-subnet --apiserver-visibility Private --ingress-visibility Private --domain aroarolatamgbb.jaropro.net --pull-secret @pull-secret.txt
+
+az aro list-credentials --name aroclusterwestus3 --resource-group arogbbwestus3
